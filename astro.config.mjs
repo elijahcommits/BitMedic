de164824ca73452250eaf9a84ee6344e41d48527
E <e@ecb>
2025-06-02 16:05:32 -0700

    Initial commit of Astro site for BitMedic!

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://elicrab.com', // Your custom domain
+  base: '/BitMedic',          // The name of your repository (the subpath)
+  // ... any other configurations like integrations
+});
