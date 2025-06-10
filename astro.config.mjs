cfbffc8b424ff62ec13d57291e11068ea7dba9cd
Elijah <14240674+elijahcommits@users.noreply.github.com>
2025-06-10 10:28:01 -0700

    Some preliminary SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  // UPDATED: Set to your custom domain
   site: 'https://www.bitmedic.io',
-
-  // UPDATED: Set base to root
   base: '/',
 });
